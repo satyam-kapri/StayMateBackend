4140cdb8aa355a54eb679e8653d71c08eb0a5519
Saniya Mehra <Saniya.Mehra@coforge.com>
2025-12-23 08:30:54 +0530

    first commit

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ app.use(morgan("dev"));
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use("/auth", authRouter);
-app.get("/ping", (req, res) => {
-  console.log("PING HIT");
-  res.send("pong");
-});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
